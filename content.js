49d45708ac1f6149f09eaddfded236148229f08c
KebabObama <lucaschyba@gmail.com>
2026-07-01 12:48:44 +0200

    reload fix

--- content.js
+++ content.js
@@ -1,25 +1,31 @@
-const selectServer =()=> {
-  const servers = document.querySelectorAll("li[link]");
-
-  for (const server of servers) {
-      if (server.textContent.trim().toLowerCase() === "vidsrc") {
-          server.click();
-          return true;
-      }
+function selectServer(root = document) {
+  if (!(root instanceof Element || root instanceof Document)) {
+      return false;
   }
-
+  const servers = root.matches?.("li[link]") ? [root] : root.querySelectorAll("li[link]");
+  for (const server of servers) {
+    if (server.textContent.trim().toLowerCase() === "vidsrc") {
+        server.click();
+        return true;
+    }
+  }
   return false;
 }
 
-const observer = new MutationObserver(() => {
-  if (selectServer()) {
-      observer.disconnect();
-  }
-});
-
-observer.observe(document.body, {
-  childList: true,
-  subtree: true
-});
-
-selectServer();
+if (!selectServer()) {
+  const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      for (const node of mutation.addedNodes) {
+        if (selectServer(node)) {
+          observer.disconnect();
+          return;
+        }
+      }
+    }
+  });
+  
+  observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+  });
+}
